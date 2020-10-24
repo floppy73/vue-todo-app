6c2ed74d72e0826c9054f89ca4727b02f34a7584
floppy73 <seisei_doda@yahoo.co.jp>
2020-10-24 19:49:44 +0900

    Make it possible to add memo space

--- src/components/Memo.vue
+++ src/components/Memo.vue
@@ -1,5 +1,28 @@
 <template>
   <v-container fluid>
+    <form @submit.prevent="submit">
+      <v-row align="center" justify="center">
+          <v-col cols="3">
+            <v-autocomplete
+              v-model="tagValues"
+              :items="tagList"
+              multiple
+              chips
+              deletable-chips
+              label="Tags"
+              prepend-inner-icon="mdi-tag"
+            ></v-autocomplete>
+          </v-col>
+          <v-col cols="8" id="content-input">
+            <v-text-field
+              v-model="memoTitleValue"
+              label="Memo Title"
+              :append-outer-icon="'mdi-plus-circle'"
+              @click:append-outer="addMemo"
+            ></v-text-field>
+          </v-col>
+      </v-row>
+    </form>
     <v-row
       v-for="item in memos"
       :key="item.title"
@@ -13,6 +36,14 @@
           class="font-weight-regular"
         >
           {{ item.title }}
+          <v-chip
+            v-for="tag in item.tags"
+            :key="tag"
+            class="ma-1"
+            color="teal lighten-5"
+          >
+            {{ tag }}
+          </v-chip>
           <v-spacer></v-spacer>
           <v-btn
             fab
@@ -29,7 +60,7 @@
           <v-textarea
             filled
             :value="item.text"
-            background-color="blue-grey lighten-5"
+            background-color="grey lighten-3"
           ></v-textarea>
         </v-card-text>
         <v-divider></v-divider>
@@ -43,21 +74,26 @@ export default {
   name: 'Memo',
   data () {
     return {
+      tagList: ['Work', 'Study', 'Math'],
+      tagValues: '',
+      memoTitleValue: '',
       memos: [
-        {title: 'Math', text: 'ベクトル復習する'},
-        {title: 'その他', text: 'アイマスを見る'}
+        {title: 'Math', tags: ['Study'], text: 'ベクトル復習する'},
+        {title: 'その他', tags: ['Hobby'], text: 'アイマスを見る'}
       ]
     }
   },
   methods: {
-    /* addMemo: function() {
-      let taskObj =  {
-        content: this.taskValue,
-        isChecked: false
+    addMemo: function() {
+      let memoObj =  {
+        title: this.memoTitleValue,
+        tags: this.tagValues,
+        text: ''
       };
       this.memos.push(memoObj);
-      this.memoValue = ""
-    }, */
+      this.memoTitleValue = "",
+      this.tagValues = ""
+    },
     deleteMemo: function(item) {
       let index = this.memos.indexOf(item);
       this.memos.splice(index, 1);
